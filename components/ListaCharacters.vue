248ecf2ef2ddca585d979ceaf6096c7ef52fba74
Luis Marin <luismgval@gmail.com>
2021-05-10 15:24:46 +0200

    style table characters

--- components/ListaCharacters.vue
+++ components/ListaCharacters.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
     <b-button variant="success" @click="getCharacters">Get Characters</b-button>
-    <b-table striped hover :items="characters"></b-table>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col">Name</th>
+          <th scope="col">Status</th>
+          <th scope="col">Species</th>
+          <th scope="col">Species</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="character in characters" :key="character.id">
+          <td><img :src="character.image" alt="" /></td>
+          <th scope="row">{{ character.name }}</th>
+          <td>{{ character.status }}</td>
+          <td>{{ character.species }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -21,4 +39,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+img {
+  width: 10%;
+}
+</style>
