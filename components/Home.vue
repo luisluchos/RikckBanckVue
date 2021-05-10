61743fd6a437ccbd34ee828bf0b7fd6f4500e283
Luis Marin <luismgval@gmail.com>
2021-05-10 13:25:31 +0200

    v1.00 PMV rickandmorty Bank

--- components/Home.vue
+++ components/Home.vue
@@ -6,7 +6,7 @@
       banco y devuélvelo cuando quieras
     </h2>
     <p class="m-5">
-      ¿ tienes cuenta? <NuxtLink to="/login">Login</NuxtLink> y si no empieza
+      ¿tienes cuenta? <NuxtLink to="/login">Login</NuxtLink> y si no, empieza
       desde ahora en <NuxtLink to="/register">Register</NuxtLink>
     </p>
   </div>
