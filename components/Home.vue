a3466d48d4656934fb607840b168879c705ff827
Luis Marin <luismgval@gmail.com>
2021-05-10 13:24:07 +0200

    v.1.02

--- components/Home.vue
+++ components/Home.vue
@@ -2,8 +2,8 @@
   <div>
     <h1 class="m-5">Rick And Morty , tu banco</h1>
     <h2>
-      Envia dinero a través del email a tus amigos, solicita un préstamo a tu
-      banco y devuelvelo cuando quieras
+      Envía dinero a través del email a tus amigos, solicita un préstamo a tu
+      banco y devuélvelo cuando quieras
     </h2>
     <p class="m-5">
       ¿ tienes cuenta? <NuxtLink to="/login">Login</NuxtLink> y si no empieza
